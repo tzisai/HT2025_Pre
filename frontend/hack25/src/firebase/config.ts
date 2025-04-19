600643de0da7c663c6de158c2c10f44d50ccffbe
Paola <montserrat_osorio@yahoo.com>
2025-04-19 11:03:19 -0600

    Cambio de onfiguracion para firebase y csv de transacciones

--- frontend/hack25/src/firebase/config.ts
+++ frontend/hack25/src/firebase/config.ts
@@ -2,13 +2,13 @@
 // NO TOCAR O NO FUNCIONARA NADAAAAA
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCDZxWisLUMlpX8McAbDLofkj0h3vP9Rv8",
-    authDomain: "sophonsdb.firebaseapp.com",
-    projectId: "sophonsdb",
-    storageBucket: "sophonsdb.firebasestorage.app",
-    messagingSenderId: "1061030064834",
-    appId: "1:1061030064834:web:9bc0043b76a78ff05c815b",
-    measurementId: "G-9843BRKMJF",
+  apiKey: "AIzaSyCQdtCHV3mdrexeV1fiBVoFlg1vq1re6jk",
+  authDomain: "sophons-db.firebaseapp.com",
+  projectId: "sophons-db",
+  storageBucket: "sophons-db.firebasestorage.app",
+  messagingSenderId: "666180820877",
+  appId: "1:666180820877:web:ce90cc8bea44d4271d27c4",
+  measurementId: "G-C8T4VB5080"
   };
   
   export default firebaseConfig;
